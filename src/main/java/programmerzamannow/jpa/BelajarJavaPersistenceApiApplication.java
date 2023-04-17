2d3802119de60dafad6bcb06235688bbb5bf606d
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-17 10:50:14 +0700

    belajar jpa

---
+++ src/main/java/programmerzamannow/jpa/BelajarJavaPersistenceApiApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaPersistenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaPersistenceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmerzamannow/jpa/BelajarJavaPersistenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaPersistenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
