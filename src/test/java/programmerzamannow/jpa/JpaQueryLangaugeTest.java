d5b2a9872bea4763a6fa544f38a64b0c97deb2fd
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-21 16:22:27 +0700

    Select Query

---
+++ src/test/java/programmerzamannow/jpa/JpaQueryLangaugeTest.java
@@ -0,0 +1,33 @@
+package programmerzamannow.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import programmerzamannow.jpa.entity.Brand;
+import programmerzamannow.jpa.util.JpaUtil;
+
+import java.util.List;
+
+public class JpaQueryLangaugeTest {
+
+    @Test
+    void select() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        TypedQuery<Brand> query = entityManager.createQuery("select b from Brand b", Brand.class);
+        List<Brand> brands = query.getResultList();
+
+        for (Brand brand : brands) {
+            System.out.println(brand.getId() + " : " + brand.getName());
+        }
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
