4180122500a2de1211e0ef48debbcd910431f94a
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-21 16:27:54 +0700

    Where Clause

--- src/test/java/programmerzamannow/jpa/JpaQueryLangaugeTest.java
+++ src/test/java/programmerzamannow/jpa/JpaQueryLangaugeTest.java
@@ -7,6 +7,7 @@ import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import programmerzamannow.jpa.entity.Brand;
+import programmerzamannow.jpa.entity.Member;
 import programmerzamannow.jpa.util.JpaUtil;
 
 import java.util.List;
@@ -25,6 +26,27 @@ public class JpaQueryLangaugeTest {
 
         for (Brand brand : brands) {
             System.out.println(brand.getId() + " : " + brand.getName());
+        }
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void whereClause() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        TypedQuery<Member> query = entityManager.createQuery("select m from Member m where " +
+                "m.name.firstName = :firstName and m.name.lastName = :lastName", Member.class);
+        query.setParameter("firstName", "Eko");
+        query.setParameter("lastName", "Khannedy");
+
+        List<Member> members = query.getResultList();
+        for (Member member : members) {
+            System.out.println(member.getId() + " : " + member.getFullName());
         }
 
         entityTransaction.commit();
